7404d68ff0c5d0306b049dbedfb875ca7f33c6bd
mixflow <wangweimf@gmail.com>
2019-06-16 20:31:58 +0800

    add createSphereVertices function in webgl-helper.js. generate sphere vertices.

--- src/webgl-helper.js
+++ src/webgl-helper.js
@@ -1,3 +1,91 @@
+/**
+ * Primitive sphere, the vertices in the list of 
+ * coordinates that is represented by the array contains 3 numbers (x,y,z).
+ * 
+ * @param {number} radius 
+ * @param {number} numVerticalSegments the number of the vertical segments. Like Earth's meridians,the line going pole to pole.
+ * @param {number} numHorizonalSegements the number of the horizonal segments. Like Earth's parallels.
+ * @param {number} [startLatitudeInRadians=0]  
+ * @param {number} [endLatitudeInRadians=Math.PI]  
+ * @param {number} [startLongitudeInRadians=0]  
+ * @param {number} [endLongitudeInRadians=2*Math.PI]  
+ */
+export function createSphereVertices(
+  radius, numVerticalSegments, numHorizonalSegements, 
+  startLatitudeInRadians = 0, endLatitudeInRadians = Math.PI, 
+  startLongitudeInRadians = 0, endLongitudeInRadians = 2 * Math.PI) {
+
+  const latitudeRange = endLatitudeInRadians - startLatitudeInRadians;
+  const longitudeRange = endLongitudeInRadians - startLongitudeInRadians;
+  
+  // add one because the end line should be two lines merged
+  const numVertices = (numVerticalSegments + 1) * (numHorizonalSegements + 1); 
+  
+  let positions = [], normals = [], texCoords = [];
+
+  for (let j = 0; j < numHorizonalSegements+1; j+=1) {
+    for (let i = 0; i < numVerticalSegments+1; i+=1) {
+      const u = i / numVerticalSegments;
+      const v = j / numHorizonalSegements;
+
+      const theta = longitudeRange * u; 
+      const phi = latitudeRange * v;
+
+      const sinTheta = Math.sin(theta), cosTheta = Math.cos(theta);
+      const sinPhi = Math.sin(phi), costPhi = Math.cos(phi);
+
+      const ux = cosTheta * sinPhi;
+      const uy = costPhi;
+      const uz = sinTheta * sinPhi;
+
+      positions.push([radius * ux, radius * uy, radius * uz]);
+      normals.push([ux, uy, uz]);
+      texCoords.push([1 - u, v]);
+    }
+  }
+
+  // generate the indices of the triangles.
+  const numVerticalSegmentVertices = numHorizonalSegements + 1;
+  let indices = [];
+  for (let i = 0; i < numVerticalSegments; i += 1) {
+    for (let j = 0; j < numHorizonalSegements; j += 1){
+      // two triangles of one face
+      /* triangle 1 
+          *1-----*2
+          |    /
+          |  / 
+          *3
+      */
+      let idx1 = [
+        j * numVerticalSegmentVertices + i,
+        j * numVerticalSegmentVertices + (i + 1),
+        (j + 1) * numVerticalSegmentVertices + i
+      ]
+      /* triangle 1 
+                 *1   
+               / |         
+            /    |
+          *2-----*3
+      */
+      let idx2 = [
+        j * numVerticalSegmentVertices + (i + 1),
+        (j + 1) * numVerticalSegmentVertices + i ,
+        (j + 1) * numVerticalSegmentVertices + (i + 1)
+      ]
+
+      indices.push(idx1, idx2);
+    }
+  }
+
+  return {
+    poistions: positions,
+    normals: normals,
+    textureCoordinates: texCoords,
+    indices: indices
+  }
+
+}
+
 /*
   GLSL shader program and GLSL code compiler.
 
