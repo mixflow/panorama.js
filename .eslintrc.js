92d430f6574a7f88e3f32e24844a5b7557cfaf59
mixflow <wangweimf@gmail.com>
2019-07-15 15:03:51 +0800

    eslint config and package.json. elsint isn't affect code at all, can ignore it if don't use lint.

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": ["eslint:recommended"],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        'no-use-before-define': 0,
+        'no-unused-vars': 1,
+        'no-console': 1,
+        'semi': 1,
+    }
+};
