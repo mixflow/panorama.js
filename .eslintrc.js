6f83c6774c45bf4a9133f70cd467a92f980a90e9
mixflow <wangweimf@gmail.com>
2019-07-15 18:11:46 +0800

    add missing jest env in eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
         "browser": true,
         "es6": true,
         "amd": true,
-        "node": true
+        "node": true,
+        "jest": true,
     },
     "extends": ["eslint:recommended"],
     "globals": {
