fcc8a003c69fc62d3ec19de78a9440b26399b0a0
mixflow <wangweimf@gmail.com>
2019-07-17 12:41:41 +0800

    add test for same effect(sin cos) degrees(e.g 450deg is equal to 90).

--- __tests__/panorama-test.js
+++ __tests__/panorama-test.js
@@ -7,12 +7,24 @@ const degRad = [
   {deg: 90, rad: Math.PI / 2},
   {deg: 180, rad: Math.PI},
   {deg: 360, rad: 2 * Math.PI},
-]
+  {deg: -45, rad: -Math.PI / 4},
+];
 
-// {deg: 450, rad: Math.PI / 2}, // same as 90
-// {deg: -90, rad: Math.PI / 2}, // same as 90
-// {deg: -270, rad: Math.PI / 2}, // same as 90
-// {deg: 450, rad: Math.PI / 2}, // same as 90
+test('some effect degrees', () => {
+  const degMappingRad = [
+    {deg: 450, rad: Math.PI / 2}, // same as 90
+    {deg: -270, rad: Math.PI / 2}, // same as 90
+    {deg: -315, rad: Math.PI / 4},
+  ];
+
+  degMappingRad.map( ({deg, rad}) => {
+    let transRad = degreeToRadian(deg);
+    expect(Math.sin(transRad)).toBeCloseTo(Math.sin(rad));
+  })
+
+});
+
+
 test('degrees to radians', () => {
   degRad.map( ( {deg, rad} ) => {
     expect(degreeToRadian(deg)).toBeCloseTo(rad);
