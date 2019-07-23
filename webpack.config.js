7a3ded43644480ff90d5a333857c52b393aeebf8
mixflow <wangweimf@gmail.com>
2019-07-23 13:02:11 +0800

    webpack add css loader and also enable module. npm install style-loader css-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,19 @@ module.exports = {
             }
           }
         ]
-      }
+      },
+      {
+        test: /\.css$/i,
+        use:[
+          'style-loader', 
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            }
+          }
+        ]
+      },
     ]
   }
 };
